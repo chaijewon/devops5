d866485d2ee7fadba550948512d7a77f794452a8
chaijewon <vcandjava@nate.com>
2025-06-18 09:49:47 +0900

    1111

--- src/main/java/com/sist/web/Devops5Application.java
+++ src/main/java/com/sist/web/Devops5Application.java
@@ -2,7 +2,11 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 서버위치 
+/*
+ *   maven => Framework => war
+ *   gradle => Boot  => jar (실행 가능 파일) 
+ */
 @SpringBootApplication
 public class Devops5Application {
 
